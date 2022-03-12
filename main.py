6b862f7527290abd7f8402ed55a55e8a7531f6fd
izikodzik <itiguuy@gmail.com>
2022-03-12 20:28:01 +0100

    Testing screen

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('UwU')
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import sh1106
+from time import sleep
+
+serial = i2c(port=1, address=0x3C)
+device = sh1106(serial, rotate=1)
+
+# Box and text rendered in portrait mode
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((10, 40), "Hello World", fill="white")
+sleep(10)
