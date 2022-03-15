5025ea7f187827dd4f9a43579f34ffc4d90da102
izikodzik <itiguuy@gmail.com>
2022-03-16 00:45:42 +0100

    Testing screen

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ w = 100
 while w > 0:
     w = w - 1
     with canvas(device) as draw:
-        draw.rectangle((0, 0, 30, 30), fill='black', outline='white', width=4)
+        draw.rectangle((0, 0, 30, 30), fill='white', outline='white', width=1)
+        draw.ellipse((30, 30, 30, 30), fill='black', outline='white')
     sleep(0.1)
 sleep(10)
