a8c3a75dc51ddabebf2c0aa246500b7ab69c61c7
izikodzik <itiguuy@gmail.com>
2022-03-16 01:09:51 +0100

    Testing connection

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ with canvas(device) as draw:
     draw.ellipse((20, 10, 30, 20), fill='white', outline='white')
     draw.ellipse((20, 30, 30, 40), fill='white', outline='white')
     draw.rectangle((10, 15, 30, 35), fill='white', width=1)
+    draw.rectangle((5, 10, 13, 15), fill='white', outline='white')
     # draw.rectangle()
     # sleep(0.1)
 while True:
