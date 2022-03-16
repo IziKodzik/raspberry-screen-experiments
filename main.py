9ef5f08dcdca84a7f3faf92fed0ef21bbcf79dc1
izikodzik <itiguuy@gmail.com>
2022-03-16 22:56:53 +0100

    Testing screen

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ with canvas(device) as draw:
     background = Image.new(device.mode, device.size, 'white')
     img_path = str(Path(__file__).resolve().parent.joinpath('images', 'eyes.png'))
     logo = Image.open(img_path).convert("RGBA")
-    background.paste(logo,0,0)
+    background.paste(logo,(0,0))
     while True:
         device.display(background)
 
