3f6d8bdfd3234bfcde9c67586689cee9fd2a72f7
izikodzik <itiguuy@gmail.com>
2022-03-16 22:50:14 +0100

    Testing screen

--- main.py
+++ main.py
@@ -19,11 +19,8 @@ with canvas(device) as draw:
     img_path = str(Path(__file__).resolve().parent.joinpath('images', 'eyes.png'))
     logo = Image.open(img_path).convert("RGBA")
     fff = Image.new(logo.mode, logo.size, (255,) * 4)
-    background = Image.new("RGBA", device.size, "black")
-    img = Image.composite(logo, fff)
+    background = Image.fromqimage(logo)
     posn = ((device.width - logo.width) // 2, 0)
     while True:
-
-        background.paste(img, posn)
-        device.display(img.convert(device.mode))
+        device.display(background)
 
